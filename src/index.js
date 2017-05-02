421a4fec5f4d37bba063585f7b1d12b112610b63
DoctorSnowman <ammonjon@gmail.com>
2017-05-02 11:21:28 -0500

    ajs - Finished YouTube Search App code from Sections 1 & 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import _ from 'lodash';
+
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = '';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('fried pickles');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        {/*<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>*/}
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
